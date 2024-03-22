1062db9f8c42cad2a3f9622581e565fb27b46b60
Chaitanya <chaitanya2692@gmail.com>
2024-03-22 21:43:49 +0100

    Switch kubeval to kubeconform

--- validate.sh
+++ validate.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/env bash
-
 set -o errexit
+set -o pipefail
+
+# mirror kustomize-controller build options
+kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
+kustomize_config="kustomization.yaml"
+
+# skip Kubernetes Secrets due to SOPS fields failing validation
+kubeconform_flags=("-skip=Secret")
+kubeconform_config=("-strict" "-ignore-missing-schemas" "-verbose")
 
 find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
@@ -9,24 +16,20 @@ find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
 done
 
 echo "INFO - Validating clusters"
-find ./clusters -type f -name '*.yaml' -maxdepth 1 -print0 | while IFS= read -r -d $'\0' file;
+find . -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
   do
-    kubeval ${file} --strict --ignore-missing-schemas
+    kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}" "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
 done
-
-# mirror kustomize-controller build options
-kustomize_flags=("--load-restrictor=LoadRestrictionsNone")
-kustomize_config="kustomization.yaml"
 
 echo "INFO - Validating kustomize overlays"
 find . -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "INFO - Validating kustomization ${file/%$kustomize_config}"
     kustomize build "${file/%$kustomize_config}" "${kustomize_flags[@]}" | \
-      kubeval --ignore-missing-schemas --strict
+      kubeconform "${kubeconform_flags[@]}" "${kubeconform_config[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
